52e9118ffc5ed6c834e5c7d1351e3221c795e3cd
SheepTester <ussa.say@gmail.com>
2020-09-19 15:51:10 -0700

    Club component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Club from './components/Club.js';
+import Colors from './Colors.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Random clubs for testing purposes */}
+      <Club
+        name="Basketball Club"
+        color={Colors.LIGHT_ORANGE}
+      />
+      <Club
+        name="Beat Production Club"
+        color={Colors.ORANGE}
+      />
+      <Club
+        name="Chess Club"
+        color={Colors.PINK}
+      />
+      <Club
+        name="Service Club"
+        color={Colors.BLUE}
+      />
     </div>
   );
 }
