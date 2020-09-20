207ec74c3b552165ab2395a2933b4c75374f544c
SheepTester <ussa.say@gmail.com>
2020-09-20 13:42:58 -0700

    Update web app name and colours (logoNNN.png and description need changing)

---
+++ src/components/Search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Search.module.css';
+
+class Search extends React.Component {
+  render() {
+    return (
+      <div className={styles.wrapper}>
+      </div>
+    );
+  }
+}
+
+export default Search;
