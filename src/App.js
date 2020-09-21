b9b0d26cd48762d543eef16b66bf510df99acba2
SheepTester <ussa.say@gmail.com>
2020-09-20 17:48:09 -0700

    Bring back looping colours

--- src/App.js
+++ src/App.js
@@ -3,14 +3,6 @@ import './App.css';
 import ClubList from './components/ClubList.js';
 import Colors from './Colors.js';
 
-const dayColors = {
-  Monday: Colors.VERY_LIGHT_ORANGE,
-  Tuesday: Colors.ORANGE,
-  Wednesday: Colors.PINK,
-  Thursday: Colors.DARK_ORANGE,
-  Friday: Colors.LIGHT_ORANGE
-};
-
 class App extends React.Component {
   state = {
     clubs: []
@@ -20,23 +12,41 @@ class App extends React.Component {
     fetch(process.env.PUBLIC_URL + '/gunn-clubs-2020-21.json')
       .then(response => response.json())
       .then(clubData => {
-        this.setState({
-          clubs: Object.entries(clubData).map(([name, {new: isNew, desc, day, time, tier}]) => {
-            const days = day.split(', ');
-            return {
-              name,
-              description: desc,
-              day,
-              time,
-              color: days.length > 1 ? Colors.BLUE : dayColors[day],
-              tags: [
-                ...days,
-                `Tier ${tier}`,
-                isNew ? 'New' : 'Returning'
-              ]
-            };
-          })
+        const clubs = Object.entries(clubData).map(([name, {new: isNew, desc, day, time, tier}]) => {
+          const days = day.split(', ');
+          return {
+            name,
+            description: desc,
+            day,
+            time,
+            tags: [
+              ...days,
+              `Tier ${tier}`,
+              isNew ? 'New' : 'Returning'
+            ]
+          };
         });
+        for (let i = 0; i < clubs.length-1; i++) {
+          if (i%4.0 === 1) {
+            clubs[i].color = Colors.ORANGE
+          }
+          else {
+            if (i%4.0 === 2) {
+              clubs[i].color = Colors.PINK
+            }
+            else {
+              if (i%4.0 === 3) {
+                clubs[i].color = Colors.BLUE
+              }
+              else {
+                if (i%4.0 === 0) {
+                  clubs[i].color = Colors.VERY_LIGHT_ORANGE
+                }
+              }
+            }
+          }
+        }
+        this.setState({ clubs });
       });
   }
 
